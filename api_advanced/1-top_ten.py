596d49ea90423d62a02dfa9c5e596d29880d6257
Clarisse-12 <c.mukayiran@alustudent.com>
2025-02-21 15:53:23 +0200

    task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,43 +1,33 @@
 #!/usr/bin/python3
-"""Write a function that queries the Reddit API
 """
-
+this module contains a function that queries the Reddit API and prints the
+titles of the first 10 hot posts listed for a given subreddit
+"""
 
 import requests
 
 
 def top_ten(subreddit):
     """
-    function that queries the Reddit API and prints the titles of the
-    first 10 hot posts listed for a given subreddit.
+    function queries the Reddit API and prints the titles of the first 10 hot
+    posts listed for a given subreddit
     """
 
-    headers = {'User-Agent': 'CustomBot/1.0'} 
-    params = {'limit': 10} 
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit) 
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    h = {'User-agent': 'ad-egg'}
+    r = requests.get(
+            url, headers=h, allow_redirects=False, params={'limit': 10})
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
-    if response.status_code == 200:
-        try:  
-            data = response.json()
-            hot_posts = data.get('data', {}).get('children', [])
-
-            if not hot_posts:
-                print(None)  
-                return
-
-            for post in hot_posts:
-                title = post.get('data', {}).get('title')
-                if title: 
-                    print(title)
-                else:
-                    print("Unknown Title") 
-
-        except (ValueError, KeyError):
-            print(None)
-            return
-
+    if r.status_code == 200:
+        req = r.json()
+        data = req.get('data')
+        children = data.get('children')
+        if data is None or children is None:
+            print('None')
+        else:
+            for post in children:
+                post_data = post.get('data')
+                title = post_data.get('title')
+                print(title)
     else:
-        print(None) 
-        return 0
+        print('None')
