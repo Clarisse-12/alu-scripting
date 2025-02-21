523cd65bf735147128f6d26fdcef2c6e4f9ecb09
Clarisse-12 <c.mukayiran@alustudent.com>
2025-02-21 13:53:54 +0200

    task 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -9,9 +9,14 @@ the number of subscribers
 (not active users, total subscribers) for a given subreddit.
 If an invalid subreddit is given, the function should return 0."""
 
+
 def number_of_subscribers(subreddit):
+    
+
     """function that queries the Reddit API and returns
 the number of subscribers"""
+
+
     headers = {'User-Agent': 'Mozilla/5.0'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
